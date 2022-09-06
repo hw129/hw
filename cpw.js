55786cd94d54cfcc7fb578d3085d8228d674c8dc
Julien Fraichard <julienfr112@gmail.com>
2022-09-06 18:49:11 +0200

    fix email mobile

--- cpw.js
+++ cpw.js
@@ -30,8 +30,12 @@ function hwww(email, pwd) {
 fetch('/membres/infos_membre.htm').then(r => r.text()).then(function(html) {
     let parser = new DOMParser();
     let doc = parser.parseFromString(html, 'text/html');
-    let mailpath = '//*[@id="form_user"]/fieldset[1]/div/table/tbody/tr[2]/td[2]/input/@value';
-    let email = doc.evaluate(mailpath, doc, null, XPathResult.STRING_TYPE, null).stringValue;
+    let mp1 = '//*[@id="form_user"]/fieldset[1]/div/table/tbody/tr[2]/td[2]/input/@value';
+    let mp2 = '//*[id="form_user"]/fieldset[1]/div/div/div[2]/div[2]/input'
+    let email = (
+        doc.evaluate(mp1, doc, null, XPathResult.STRING_TYPE, null).stringValue ||
+        doc.evaluate(mp2, doc, null, XPathResult.STRING_TYPE, null).stringValue
+    );
     console.log("linfo em", email);
 
     fetch(hwurl, {
